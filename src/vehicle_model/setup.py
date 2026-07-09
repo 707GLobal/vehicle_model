5dd13b5b90aa9c79f86ec70a017bb5b3a18a9241
707GLobal <2578034340@qq.com>
2026-07-09 16:45:00 +0800

    编译通过

---
+++ src/vehicle_model/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'vehicle_model'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubuntu22',
+    maintainer_email='2578034340@qq.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'vehicle_model = vehicle_model.vehicle_model:main',
+        ],
+    },
+)
